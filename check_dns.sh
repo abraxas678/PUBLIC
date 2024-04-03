09acba56abf772692748e5a632e7d63bd84a6268
abraxas678 <abraxas678@gmail.com>
2024-04-03 21:04:56 +0200

    1

--- check_dns.sh
+++ check_dns.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd $HOME
-ping -c 1 google.com >/dev/null && echo "Online" || echo "Offline"
-ping -c 1 google.com >/dev/null && ONL=1 || ONL=0
+  CHECK=$(cat /etc/resolv.conf)
+  if [[ $CHECK != *"8.8.8.8"* ]] ; then
+    ONL=0
+  else
+    ping -c 1 google.com >/dev/null && echo "Online" || echo "Offline"
+    ping -c 1 google.com >/dev/null && ONL=1 || ONL=0
+  fi
 if [[ $ONL = "0" ]]; then
   CHECK=$(cat /etc/resolv.conf)
   if [[ $CHECK != *"8.8.8.8"* ]] ; then
