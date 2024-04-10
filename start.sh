882b1944f53ca90332f0b70fb693aa21ea650bcd
abraxas678 <abraxas678@gmail.com>
2024-04-10 09:52:29 +0200

    1

--- start.sh
+++ start.sh
@@ -209,6 +209,12 @@ mybashhub() {
 }
 mybashhub
 
+brew() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zprofile && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+}
+echo; echo "BREW"
+brew
+
 mount_nc() {
   echo
   header2 MOUNTING NEXTCLOUD
