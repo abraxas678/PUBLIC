13411dbd3c0ba36474d2ade66713506ac986e23c
abraxas678 <abraxas678@gmail.com>
2024-04-14 06:11:09 +0200

    1

--- start.sh
+++ start.sh
@@ -225,6 +225,15 @@ mount_folder() {
   sshfs abrax@192.168.11.162:/var/www/nextcloud/data/abraxas678/files/LINUX/abrax/.config /home/abrax/.config
 }
 
+zsh4humans() {
+if command -v curl >/dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+else
+  sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+fi
+}
+zsh4humans
+
 exit
 
 
