6354c8e43bc15886200cd37542ad8492523d1118
abraxas678 <abraxas678@gmail.com>
2024-04-28 12:03:37 +0200

    Update functionality and fix bugs in several modules

--- check.sh
+++ check.sh
@@ -4,7 +4,22 @@ if [[ ! -d $HOME/tmp/public ]]; then
   cd $HOME/tmp
   git clone git@github.com:abraxas678/public.git
 fi
+
+if [[ ! -d $HOME/tmp/stats ]]; then
+  mkdir -p $HOME/tmp
+  cd $HOME/tmp
+  git clone git@github.com:abraxas678/stats.git
+fi
+
 cd $HOME/tmp/public
 git pull
 [[ $? != 0 ]] && exit
-df -h  | awk '{print %5}'
+
+cd $HOME/tmp/stats
+git pull
+[[ $? != 0 ]] && exit
+
+stats_dir="$HOME/tmp/stats"
+output_file="$stats_dir/df$(hostname).stats"
+df -h | grep -v Use > "$output_file"
+
