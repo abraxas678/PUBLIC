5a74072fc0ec7815f07ae2170b178e343b174f2b
abraxas678 <abraxas678@gmail.com>
2024-04-28 11:51:52 +0200

    Update functionality and fix bugs in several modules

--- check.sh
+++ check.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+if [[ ! -d $HOME/tmp/public ]]; then
+  mkdir -p $HOME/tmp
+  cd $HOME/tmp
+  git clone git@github.com:abraxas678/public.git
+fi
+cd $HOME/tmp/public
+git pull
+[[ $? != 0 ]] && exit
+df -h  | awk '{print %5}'
