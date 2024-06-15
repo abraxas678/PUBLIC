121b253c467fe470c4c7da484eb7ac4a0d40cfeb
abraxas678 <abraxas678@gmail.com>
2024-06-15 18:45:27 +0200

    1

--- start.sh
+++ start.sh
@@ -139,7 +139,7 @@ mkdir ~/tmp -p
 MYPWD=$PWD
 cd $HOME/tmp
 sudo apt update && sudo apt install unzip -y
-[[ ! -f $HOME/tmp/Terminus.zip ]] && [[ ! -d $HOME/tmp/Terminus ]] && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Terminus.zip && unzip Terminus.zip && sudo mv *.ttf /usr/share/fonts 
+[[ ! -f $HOME/tmp/Terminus.zip ]] && [[ ! -d $HOME/tmp/Terminus ]] && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Terminus.zip && unzip Terminus.zip && sudo mv *.ttf /usr/share/fonts/truetype && sudo fc-cache -fv 
 #echo user1
 TASK "CHECK: USER = abrax? "
 # Check if user is not abrax, if not then switch to abrax
