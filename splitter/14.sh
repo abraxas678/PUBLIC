aabcdf95b83791b4fb64bcd9cea5f74b45b34a52
abraxas678 <abraxas678@gmail.com>
2024-06-26 22:04:55 +0200

    1

--- splitter/14.sh
+++
@@ -1,7 +0,0 @@
-y#!/bin/bash
-################14. GitHub Repository Check and Update   DELETE
-# Check GitHub repository for updates
-
-GITSTATUS=$(git status)
-echo $GITSTATUS
-[[ $GITSTATUS != *"nothing to commit"* ]] && git add . && git commit -m "Auto-update" && git push
--- splitter/4.sh
+++ splitter/4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#####################4. User setup
+#######################4. User setup
 # Task: Check if user is 'abrax'
 TASK "CHECK: USER = abrax?"
 if [[ $USER != *"abrax"* ]]; then
