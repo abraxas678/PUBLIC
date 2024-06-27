90fef9eebb5f743e55b278ca5b67fc8839b772d9
abraxas678 <abraxas678@gmail.com>
2024-06-27 06:28:04 +0200

    move_and_rename.sh

--- splitter/move_and_rename.sh
+++ splitter/move_and_rename.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+rich -u -p "git"
 git add --all
 git commit -a -m "move_and_rename.sh"
 git push
 
+rich -u -p "collect header"
 ./collect_header.sh
+rich -u -p "sed empty lines"
 sed '/./d' index.txt >index2.txt
+rich -u -p "while loop
+"
 while IFS= read -r line; do
   echo $line
   echo $line  | awk '{print $1}' 
