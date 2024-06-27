9c2d821b2f076e5ab87d280b567faa17361c0ca7
abraxas678 <abraxas678@gmail.com>
2024-06-27 10:30:14 +0200

    move_and_rename.sh

--- splitter/5.sh
+++ splitter/5.sh
@@ -1,21 +1,9 @@
 !/bin/bash
-5. APT update -- check last update time  SHOULD BE 3
+3. Check Machine Name
 
- Task: Check last apt update time
-TASK "check last update time"
-ts=$(date +%s)
-
-if [[ -f ~/last_apt_update.txt ]]; then
-  DIFF=$(($ts - $(cat ~/last_apt_update.txt)))
-  if [[ $DIFF -gt 6000 ]]; then
-    sudo apt update && sudo apt upgrade -y
-  fi
-else
-  sudo apt update && sudo apt upgrade -y
-fi
-
-echo $ts > ~/last_apt_update.txt
-
-sudo apt install -y unzip curl wget nano
-
-[[ ! -f Terminus.zip ]] && [[ ! -d Terminus ]] && wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Terminus.zip && unzip Terminus.zip && sudo mv *.ttf /usr/share/fonts/truetype && sudo fc-cache -fv
+ Change machine name
+header2 "change machine name"
+cd $HOME
+curl -sL machine.yyps.de > machine.sh
+chmod +x machine.sh
+./machine.sh
