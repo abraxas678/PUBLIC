7c44510b92cc9c9918a9faed70e7b2797e1aea05
abraxas678 <abraxas678@gmail.com>
2024-06-27 10:51:39 +0200

    move_and_rename.sh

--- splitter/collect_header.sh
+++ splitter/collect_header.sh
@@ -4,6 +4,9 @@ x=$(ls *.sh | wc -l)
 cp  $HOME/tmp/public/splitter/index.txt   $HOME/tmp/public/splitter/index$ts.txt 
 mv  $HOME/tmp/public/splitter/index.txt   $HOME/tmp/public/splitter/tmp.txt 
 y=1
+y2=$(printf "%02d" $y) 
+echo $y $y2
+exit
 while [[ "$y" -lt "$((x+1))" ]]; do
   [[ ${#y} = 1 ]] && y2=$(printf "%02d" $y) && mv $y.sh $y2.sh && y=$(printf "%02d" $y) 
   TITLE=$(cat $HOME/tmp/public/splitter/$y.sh | head -n 2 | tail -n 1)
