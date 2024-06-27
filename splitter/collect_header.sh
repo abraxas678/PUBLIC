0bf927e7beb00d6937f55de23327821ff86f0eee
abraxas678 <abraxas678@gmail.com>
2024-06-27 09:49:10 +0200

    move_and_rename.sh

--- splitter/collect_header.sh
+++ splitter/collect_header.sh
@@ -6,7 +6,7 @@ mv  $HOME/tmp/public/splitter/index.txt   $HOME/tmp/public/splitter/tmp.txt
 y=1
 while [[ "$y" -lt "$((x+1))" ]]; do
   TITLE=$(cat $HOME/tmp/public/splitter/$y.sh | head -n 2 | tail -n 1)
-  echo $TITLE | sed 's/#//g'>>$HOME/tmp/public/splitter/index.txt
+  echo $TITLE | sed 's/##//g'>>$HOME/tmp/public/splitter/index.txt
   sed 's/##//g' -i $y.sh
   sed "s/$TITLE/#$TITLE/" -i $y.sh
   y=$((y+1))
