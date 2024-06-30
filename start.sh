97fa58d9dc9267910cd3ae55504ca293e4b92076
abraxas678 <abraxas678@gmail.com>
2024-06-30 21:30:06 +0200

    1

--- start.sh
+++ start.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+cd $HOME
+mkdir tmp -p
+cd tmp
+
+[[ ! -d splitter ]] && git clone https://github.com/abraxas678/splitter
+cd splitter
+git pull
+chmod +x *.sh
+./create_script.sh
+
+
+exit
 curl -L start-main.yyps.de >start-main.sh
 chmod +x start-main.sh
 ./start-main.sh
