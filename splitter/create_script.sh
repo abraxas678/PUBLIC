759a83431df10ba3eabcad897969b08dc961b8ea
abraxas678 <abraxas678@gmail.com>
2024-06-30 15:39:46 +0200

    1

--- splitter/create_script.sh
+++ splitter/create_script.sh
@@ -3,10 +3,11 @@
 ls *.sh >myfiles
 
 while IFS= read -r line; do
+     clear
      echo include the followinfg script?
      echo "$line  " 
      rich -p "$(cat $line | head -n 22)" -a rounded -s blue
-     read me
+     read -p "y/n >> " me
 #    echo $line
 #    sgpt --model ollama/llama3 "explain what this script does in one short sentance: $(cat $line)" >$line.desc
 #    [[ ! -f $line.desc ]] && ollama run llama3  "explain what this script does in one short sentance: $(cat $line)" >$line.desc
