89dae1a75f75cf858d43b8771bdd5272a10239ed
abraxas678 <abraxas678@gmail.com>
2024-07-31 20:45:14 +0200

    1

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ curl -fsSL https://tailscale.com/install.sh | sh
 git config --global user.email "abraxas678@gmail.com"
 git config --global user.name "abraxas678"
 sudo -v ; curl https://rclone.org/install.sh | sudo bash -s beta
-gh auth login
+[[ $(gh auth status) != *"Logged in to github.com account abraxas678"* ]] && gh auth login
 
 cd $HOME
 [[ ! -d webapps ]] && gh repo clone webapps
@@ -18,7 +18,7 @@ cd $HOME
 source /home/abrax/bin/header.sh
 [[ ! -d tmpconfig ]] && gh repo clone .config tmpconfig
 rclone move tmpconfig/ .config/ --update -P
-rm -rf tmpconfig
+#rm -rf tmpconfig
 
 pip install rich-cli
 [[ $? != 0 ]] && [[ $(pipx list) != *"- rich"* ]] && pipx install rich-cli && pipx ensurepath && exec bash
@@ -33,6 +33,9 @@ cd $HOME
 #cp 02_fix_letter_or_number.sh /home/abrax/bin/letter_or_number.sh 
 #source 01_fix_create_scripts.sh
 #source ./create_script.sh
+echo
+echo script_runner
+echo
 /bin/bash $HOME/webapps/script_runner/script_runner.sh
 
 exit
