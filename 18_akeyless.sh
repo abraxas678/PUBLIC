72b5957675e578c4b56a44d37774cd41c7b96997
abraxas678 <abraxas678@gmail.com>
2024-11-10 02:29:21 +0100

    1

--- 18_akeyless.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#04. akeyless setup
-curl -o akeyless https://akeyless-cli.s3.us-east-2.amazonaws.com/cli/latest/production/cli-linux-amd64
-chmod +x akeyless
-mv akeyless /home/abrax/bin/
-echo; echo; echo "ACCESS ID p-z887athlsakpim"; echo; echo
-/home/abrax/bin/akeyless configure --access-id p-mcidcla45c0cam --access-type oidc --profile 'github-oidc'
