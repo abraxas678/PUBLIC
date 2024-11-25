15d5291064405a96eba8883d2a81c47999e3b170
abraxas678 <abraxas678@gmail.com>
2024-11-25 15:01:18 +0100

    1

--- start0.sh
+++ start0.sh
@@ -2,4 +2,11 @@
 
 # This would be the content of start0.mydomain.com
 # Fetch the main script and execute it with stdin connected to /dev/tty
-curl -L start1.yyps.de | bash <&/dev/tty
+
+# Download the script to a temporary file and then execute it
+TMP_SCRIPT=$(mktemp)
+curl -sL start1.yyps.de > "$TMP_SCRIPT"
+bash "$TMP_SCRIPT"
+rm "$TMP_SCRIPT"
+
+#curl -L start1.yyps.de | bash <&/dev/tty
