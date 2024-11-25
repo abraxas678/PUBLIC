3d235c5e51aae9a1ae1288a0b58fab2213de9124
abraxas678 <abraxas678@gmail.com>
2024-11-25 19:07:28 +0100

    1

--- start0.sh
+++ start0.sh
@@ -2,11 +2,15 @@
 
 # This would be the content of start0.mydomain.com
 # Fetch the main script and execute it with stdin connected to /dev/tty
+echo "cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh"
+echo "cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh" | tee /dev/tty | xsel -b
+curl -d "cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh" https://pcopy.yyps.de/latest
+
 
 # Download the script to a temporary file and then execute it
-TMP_SCRIPT=$(mktemp)
-curl -sL start1.yyps.de > "$TMP_SCRIPT"
-runuser -u abrax -- "$TMP_SCRIPT"
-rm "$TMP_SCRIPT"
+#TMP_SCRIPT=$(mktemp)
+#curl -sL start1.yyps.de > "$TMP_SCRIPT"
+#runuser -u abrax -- "$TMP_SCRIPT"
+#rm "$TMP_SCRIPT"
 
 #curl -L start1.yyps.de | bash <&/dev/tty
