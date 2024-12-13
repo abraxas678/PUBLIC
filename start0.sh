cd690bd193b9faeeaa0ff2d3375ad2fb276343aa
abraxas678 <abraxas678@gmail.com>
2024-12-13 12:49:40 +0100

    Update start0.sh

--- start0.sh
+++ start0.sh
@@ -2,12 +2,17 @@
 
 # This would be the content of start0.mydomain.com
 # Fetch the main script and execute it with stdin connected to /dev/tty
+clear
 echo
 echo 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh'
 echo
+apt update >/dev/null 2>&1 
+apt install xsel -y >/dev/null 2>&1 
 echo 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh' | tee /dev/tty | xsel -b
 curl -d 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh' https://pcopy.yyps.de/latest
-
+wget https://github.com/binwiederhier/pcopy/releases/download/v0.6.1/pcopy_0.6.1_amd64.deb
+sudo apt install -y ./pcopy_0.6.1_amd64.deb >/dev/null 2>&1 
+echo ppaste
 
 # Download the script to a temporary file and then execute it
 #TMP_SCRIPT=$(mktemp)
