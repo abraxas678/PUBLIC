064e7e41d9d29484e7bc9536bf8d4d8eb03401c1
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2024-12-16 09:16:10 +0100

    1

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@ read -p B me
 echo $me
 
 # Validate the API key is not empty
-#while [[ -z "$BWS_API_KEY" ]]; do
-#  echo -e "\e[1;31mAPI key cannot be empty\e[0m"
-#  echo -e "\e[1;33mPlease enter your Bitwarden API key:\e[0m"
-#  read -s -t 60 BWS_API_KEY
-#  echo
-#  clear
-#done
+while [[ -z "$BWS_API_KEY" ]]; do
+  echo -e "\e[1;31mAPI key cannot be empty\e[0m"
+  echo -e "\e[1;33mPlease enter your Bitwarden API key:\e[0m"
+  read -s -t 3 BWS_API_KEY
+  echo
+  clear
+done
 
