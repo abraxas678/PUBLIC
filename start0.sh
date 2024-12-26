b5fb81dcab519b440687a5c1421487586fc55e69
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2024-12-26 19:12:38 +0100

    1

--- start0.sh
+++ start0.sh
@@ -12,9 +12,9 @@ echo
 #echo "Checking if xsel is installed..."
 [[ $(command -v xsel) ]] || { echo "Installing xsel..."; $MYSUDO apt install xsel -y >/dev/null 2>&1; }
 echo "Checking if pcopy is installed..."
-
-
-wget internal.yyps.de/pcopy/pcopy.tar
+echo
+wget -s internal.yyps.de/pcopy/pcopy.tar
+echo
 echo 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh' | tee /dev/tty | xsel -b
 curl -d 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh' https://pcopy.yyps.de/latest
 echo
@@ -23,7 +23,7 @@ echo
   wget https://github.com/binwiederhier/pcopy/releases/download/v0.6.1/pcopy_0.6.1_amd64.deb >/dev/null 2>&1 
   echo
   sudo apt install -y ./pcopy_0.6.1_amd64.deb >/dev/null 2>&1
-  pcopy join 
+  pcopy join https://pcopy.yyps.de 
 }
 echo 'cd; curl -L start1.yyps.de >s.sh; chmod +x s.sh; ./s.sh' | pcp >del 2>&1
 
