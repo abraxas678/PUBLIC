2187b01c16f147907fa58c5ab093f4a3fea5f196
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-01-25 04:09:28 +0100

    git commit -m "Aktualisierte Konfiguration undkorrigierte Fehler"

--- versioning.sh
+++ versioning.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 #curl -d versioning https://n.yyps.de/alert
 curl -d "$(echo $semaphore_vars)" https://n.yyps.de/alert
+echo 
+echo semaphore_vars
+echo $semaphore_vars
+echo semaphore_vars
+echo
 echo 1
 echo 2
 echo 3
