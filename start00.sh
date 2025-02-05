2c43ff47c6d6e76230e98f6a57e57a23c9bcdb2d
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-02-05 01:51:26 +0100

    1

--- start00.sh
+++ start00.sh
@@ -2,14 +2,26 @@
 [[ $(whoami) = "root" ]] && MYSUDO="" || MYSUDO="sudo"
 $MYSUDO apt update
 
+mkdir -p $HOME/tmp
+cd $HOME/tmp
 command gum -v >/dev/null 2>&1
 if [[ $? != 0 ]]; then
-  /home/abrax/tmp/public/gum_install.sh
+  wget https://raw.githubusercontent.com/abraxas678/public/refs/heads/master/gum_install.sh
+  chmod +x gum_install.sh
+  ./gum_install.sh
 fi
 
+mkdir -p ~/.ssh
+BWS="$(gum input --password --no-show-help --placeholder='enter bws.dat')"
+echo $BWS >~/.ssh/bws.dat
 
-
-
+command bws --version >/dev/null 2>&1;
+STAT=$(echo $?)
+if [[ $STAT != 0 ]]; then
+  wget https://github.com/abraxas678/public/raw/refs/heads/master/bws_install.sh
+  chmod +x bws_install.sh
+  ./bws_install.sh
+fi
 
 bws run -- git config --global user.email "$MYEMAIL"
 bws run -- git config --global user.name "$GITHUB_USERNAME"
@@ -23,22 +35,10 @@ else
   git pull
 fi
 
-command bws --version >/dev/null 2>&1;
-STAT=$(echo $?)
-echo STAT $STAT
-sleep 2
-if [[ $STAT != 0 ]]; then
-  $HOME/tmp/public/bws_install.sh
-fi
-
-mkdir -p ~/.ssh
-BWS="$(gum input --password --no-show-help --placeholder='enter OTP')"
-#read -p "bws.dat: >> " BWS
-echo $BWS >~/.ssh/bws.dat
 command chezmoi -v >/dev/null 2>&1
 STAT="$(echo $?)"
 if [[ $STAT != 0 ]]; then
-  bws run -- sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+  bws run -- 'sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME'
 fi
 
 #41bff4b2-2ccb-42ba-b33a-b27a00ba0f50
