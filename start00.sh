9689eddee692d7f44d394987799f97228cd8b766
380f19b0-171a-4cd5-a826-b24601628d1d <abraxas678@gmail.com>
2025-02-05 01:41:06 +0100

    1

--- start00.sh
+++ start00.sh
@@ -7,12 +7,28 @@ else
   cd $HOME/tmp/public
   git pull
 fi
+
+command gum -v >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+  /home/abrax/tmp/public/gum_install.sh
+fi
+
 command bws --version >/dev/null 2>&1;
 STAT=$(echo $?)
 echo STAT $STAT
 sleep 2
 if [[ $STAT != 0 ]]; then
   $HOME/tmp/public/bws_install.sh
+fi
+
+mkdir -p ~/.ssh
+BWS="$(gum input --password --no-show-help --placeholder='enter OTP')"
+#read -p "bws.dat: >> " BWS
+echo $BWS >~/.ssh/bws.dat
+command chezmoi -v >/dev/null 2>&1
+STAT="$(echo $?)"
+if [[ $STAT != 0 ]]; then
+  bws run -- sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 fi
 
 #41bff4b2-2ccb-42ba-b33a-b27a00ba0f50
