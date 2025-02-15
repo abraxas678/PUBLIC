fd868b7ae56ffa25d614f82e775102395247d5b7
380f19b0-171a-4cd5-a826-b24601628d1d <9d393c6c-b12c-4be5-a62c-b226006c7185>
2025-02-15 08:36:19 +0100

    1

--- start0.sh
+++ start0.sh
@@ -2,6 +2,20 @@
 
 [[ $(whoami) = "root" ]] && MYSUDO="" || MYSUDO="sudo"
 
+echothis() {
+  echo;  gum spin --spinner="pulse" --title="" --spinner.foreground="33" --title.foreground="33" sleep 1
+  echo -e "\e[1;38;5;34m╭─ \e[1;38;5;39m$@\e[0m"
+  echo -e "\e[1;38;5;34m╰─ \e[2;38;5;245m[$(date +%H:%M:%S)]\e[0m"
+  gum spin --spinner="pulse" --title="" --spinner.foreground="33" --title.foreground="33" sleep 1
+  for i in {1..3}; do
+     gum spin --spinner="dot" --title=".$(printf '%0.s.' $(seq 1 $i))" --spinner.foreground="33" --title.foreground="33" sleep 0.1
+  done
+}
+
+echothis2() {
+  echo -e "\e[1;36m└─ 󰄬 $1 installation completed\e[0m"
+}
+
 isinstalled() {
   echo "[[isinstalled()]]"
   echo " [$1]"
@@ -40,6 +54,16 @@ fi
 isinstalled curl
 
 cd $HOME/tmp/public
+
+which tabby >/dev/null 2>&1
+if [[ $? != 0 ]]; then
+  echothis "install tabby"
+  URL="$($HOME/tmp/public/github_latest_release_url.sh Eugeny tabby)"
+  cd $HOME/tmp
+  wget $URL
+  $MYSUDO apt install $HOME/tmp/$(basename $URL)
+fi
+
 echo
 echo "===================================================="
 echo "___>>> EXECUTE "
