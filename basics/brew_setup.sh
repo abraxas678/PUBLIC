85bb4a43fae7c9798808efc9f7ba185d40a68511
380f19b0-171a-4cd5-a826-b24601628d1d <9d393c6c-b12c-4be5-a62c-b226006c7185>
2025-05-12 23:59:02 +0200

    1

--- basics/brew_setup.sh
+++ basics/brew_setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+echo >> /home/abrax/.zshrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/abrax/.zshrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    sudo apt-get install build-essential
+    brew install gcc
