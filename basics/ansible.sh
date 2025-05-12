2cd4aa5238c827abc20c9304c18901e61e16f60b
abraxas678 <abraxas678@gmail.com>
2025-05-12 20:56:47 +0000

    1

--- basics/ansible.sh
+++ basics/ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook ~/bin/setup_ansible_apps.yml --ask-become-pass -i ~/.config/ansible/inventory.ini
+ansible-playbook setup_ansible_apps.yml --ask-become-pass -i inventory.ini
